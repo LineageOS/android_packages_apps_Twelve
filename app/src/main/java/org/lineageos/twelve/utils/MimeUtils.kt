607a3eac7b0a3865fd90f94aad804513e0d6aef7
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-11-06 19:20:32 +0100

    Twelve: Initial intents handling
    Do everything on a view model, activities will react on parsed intents
    Our MainActivity will only handle "folders"
    
    Change-Id: Id124d588c2cdf3e449623b45864c33e60cfe1c55

--- app/src/main/java/org/lineageos/twelve/utils/MimeUtils.kt
+++ app/src/main/java/org/lineageos/twelve/utils/MimeUtils.kt
@@ -7,6 +7,7 @@ package org.lineageos.twelve.utils
 
 import androidx.media3.common.MimeTypes
 import androidx.media3.common.util.UnstableApi
+import org.lineageos.twelve.datasources.MediaType
 
 object MimeUtils {
     @androidx.annotation.OptIn(UnstableApi::class)
@@ -14,5 +15,24 @@ object MimeUtils {
         it.takeIf { it.contains('/') }
             ?.substringAfterLast('/')
             ?.uppercase()
+    }
+
+    fun mimeTypeToMediaType(mimeType: String) = when {
+        mimeType.startsWith("audio/") -> MediaType.AUDIO
+
+        else -> when (mimeType) {
+            "application/itunes",
+            "application/ogg",
+            "application/x-ogg",
+            "vnd.android.cursor.item/audio" -> MediaType.AUDIO
+
+            "vnd.android.cursor.item/album" -> MediaType.ALBUM
+
+            "vnd.android.cursor.item/artist" -> MediaType.ARTIST
+
+            "vnd.android.cursor.item/playlist" -> MediaType.PLAYLIST
+
+            else -> null
+        }
     }
 }
