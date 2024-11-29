888dd2d634d01f175dfb98ae33eb59d4c9789722
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-11-29 21:13:34 +0100

    [DNM]Twelve: Add ktor
    Sane http client based on okhttp
    Broken because of kotlin serialization deps
    
    Change-Id: I794a652b17b0f5504e925284be737b5bba69c73f

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -112,7 +112,12 @@ dependencies {
     implementation(libs.nier.visualizer) {
         exclude(group = "com.android.support")
     }
+
     implementation(libs.okhttp)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
 }
 
 configure<GenerateBpPluginExtension> {
