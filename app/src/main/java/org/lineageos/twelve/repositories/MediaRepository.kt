13d83cf8ac1d9ad0f6f60da1190f5f1ec1c20985
Joey <joey@lineageos.org>
2025-01-04 15:33:55 +0100

    Twelve: Track audio stats and last played information
    - Count the number of time local audio files are are played
    - GC the counts so that we don't keep no-longer existing entries
      when the app starts
    - Save information about the last played audio for all sources
    
    Change-Id: I20d3f29c3b92250c333406e185bab34085398a41

--- app/src/main/java/org/lineageos/twelve/repositories/MediaRepository.kt
+++ app/src/main/java/org/lineageos/twelve/repositories/MediaRepository.kt
@@ -18,11 +18,13 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import okhttp3.Cache
 import org.lineageos.twelve.database.TwelveDatabase
 import org.lineageos.twelve.datasources.JellyfinDataSource
@@ -98,7 +100,11 @@ class MediaRepository(
                     ProviderType.SUBSONIC,
                     it.id,
                     it.name,
-                ) to SubsonicDataSource(arguments, cache)
+                ) to SubsonicDataSource(
+                    arguments,
+                    database,
+                    cache,
+                )
             }
         },
         database.getJellyfinProviderDao().getAll().mapLatest { jellyfinProviders ->
@@ -117,6 +123,10 @@ class MediaRepository(
                     database.getJellyfinProviderDao().getToken(it.id)
                 }, { token ->
                     database.getJellyfinProviderDao().updateToken(it.id, token)
+                }, { datasource ->
+                    database.getLastPlayedDao().get(datasource)
+                }, { datasource, uri ->
+                    database.getLastPlayedDao().set(datasource, uri)
                 }, cache)
             }
         }
@@ -182,6 +192,10 @@ class MediaRepository(
             SharingStarted.Eagerly,
             localProvider,
         )
+
+    init {
+        scope.launch { gcLocalMediaStats() }
+    }
 
     /**
      * Given a media item, get a flow of the provider that handles these media items' URIs.
@@ -511,6 +525,14 @@ class MediaRepository(
         }
 
     /**
+     * @see MediaDataSource.onAudioPlayed
+     */
+    suspend fun onAudioPlayed(audioUri: Uri) =
+        withMediaItemsDataSource(audioUri) {
+            onAudioPlayed(audioUri)
+        }
+
+    /**
      * Get a flow of the [MediaDataSource] associated with the given [Provider].
      *
      * @param providerType The [ProviderType]
@@ -594,5 +616,20 @@ class MediaRepository(
         val defaultPlaylistsSortingRule = SortingRule(
             SortingStrategy.MODIFICATION_DATE, true
         )
+    }
+
+    /**
+     * Remove items that are no longer in the local data source from the local media stats table.
+     */
+    private suspend fun gcLocalMediaStats() {
+        val statsDao = database.getLocalMediaStatsProviderDao()
+        val allStats = statsDao.getAll()
+        val inSource = localDataSource.audios().mapLatest { it }.first()
+
+        allStats.forEach {
+            if (inSource.none { audio -> audio.playbackUri == it.mediaUri }) {
+                statsDao.delete(it.mediaUri)
+            }
+        }
     }
 }
