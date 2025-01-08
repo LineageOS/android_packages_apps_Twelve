140f2ef70e46b6a89a8889cb973544571242db48
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-01-08 16:51:22 +0100

    Twelve: Introduce speed/pitch playback control
    Change-Id: I8f66008ef99a28f66b12167512252ab52f49bef1

---
+++ app/src/main/java/org/lineageos/twelve/viewmodels/PlaybackControlViewModel.kt
@@ -0,0 +1,81 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.twelve.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.viewModelScope
+import androidx.media3.common.PlaybackParameters
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.stateIn
+import org.lineageos.twelve.ext.playbackParametersFlow
+import org.lineageos.twelve.ext.withPitch
+
+class PlaybackControlViewModel(application: Application) : TwelveViewModel(application) {
+    private val _disableSpeedPlus = MutableStateFlow(false)
+    val disableSpeedPlus = _disableSpeedPlus
+
+    private val _disableSpeedMinus = MutableStateFlow(false)
+    val disableSpeedMinus = _disableSpeedMinus
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val playbackParameters = mediaController
+        .filterNotNull()
+        .flatMapLatest { it.playbackParametersFlow() }
+        .flowOn(Dispatchers.Main)
+        .stateIn(
+            viewModelScope,
+            started = SharingStarted.WhileSubscribed(),
+            initialValue = PlaybackParameters(1f, 1f)
+        )
+
+    fun increasePlaybackSpeed() {
+        val newSpeed = playbackParameters.value.speed + SPEED_STEP
+        if (newSpeed <= SPEED_MAX) {
+            mediaController.value?.setPlaybackParameters(
+                playbackParameters.value.withSpeed(newSpeed)
+            )
+        }
+        _disableSpeedPlus.value = newSpeed >= SPEED_MAX
+    }
+
+    fun decreasePlaybackSpeed() {
+        val newSpeed = playbackParameters.value.speed - SPEED_STEP
+        if (newSpeed >= SPEED_MIN) {
+            mediaController.value?.setPlaybackParameters(
+                playbackParameters.value.withSpeed(newSpeed)
+            )
+        }
+        _disableSpeedMinus.value = newSpeed <= SPEED_MIN
+    }
+
+    fun resetPlaybackSpeed() {
+        mediaController.value?.setPlaybackParameters(
+            playbackParameters.value.withSpeed(SPEED_DEFAULT)
+        )
+
+        _disableSpeedPlus.value = false
+        _disableSpeedMinus.value = false
+    }
+
+    fun setPlaybackPitch(pitch: Float) {
+        mediaController.value?.setPlaybackParameters(
+            playbackParameters.value.withPitch(pitch)
+        )
+    }
+
+    companion object {
+        private const val SPEED_DEFAULT = 1f
+        private const val SPEED_MIN = 0.5f
+        private const val SPEED_MAX = 2.5f
+        private const val SPEED_STEP = 0.25f
+    }
+}
