e4d5ab10349d73ea5b046666cd1adee9d70c54ea
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-01-08 16:51:22 +0100

    Twelve: Introduce speed/pitch playback control
    Change-Id: I8f66008ef99a28f66b12167512252ab52f49bef1

---
+++ app/src/main/java/org/lineageos/twelve/viewmodels/PlaybackControlViewModel.kt
@@ -0,0 +1,45 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.twelve.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.viewModelScope
+import androidx.media3.common.PlaybackParameters
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.stateIn
+import org.lineageos.twelve.ext.playbackParametersFlow
+import org.lineageos.twelve.ext.withPitch
+
+class PlaybackControlViewModel(application: Application) : TwelveViewModel(application) {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val playbackParameters = mediaController
+        .filterNotNull()
+        .flatMapLatest { it.playbackParametersFlow() }
+        .filterNotNull()
+        .flowOn(Dispatchers.Main)
+        .stateIn(
+            viewModelScope,
+            started = SharingStarted.WhileSubscribed(),
+            initialValue = PlaybackParameters(1f, 1f)
+        )
+
+    fun setPlaybackSpeed(speed: Float) {
+        mediaController.value?.setPlaybackParameters(
+            playbackParameters.value.withSpeed(speed)
+        )
+    }
+
+    fun setPlaybackPitch(pitch: Float) {
+        mediaController.value?.setPlaybackParameters(
+            playbackParameters.value.withPitch(pitch)
+        )
+    }
+}
