99283ed8a11fb4b81f0179f4eddfdb6283eb7e4f
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-01-22 16:57:11 +0100

    Twelve: Make MediaController a singleton
    Change-Id: If2f71b50a8d806ed69c83f59df25e2386dde6de9

--- app/src/main/java/org/lineageos/twelve/TwelveApplication.kt
+++ app/src/main/java/org/lineageos/twelve/TwelveApplication.kt
@@ -6,15 +6,23 @@
 package org.lineageos.twelve
 
 import android.app.Application
+import android.content.ComponentName
 import androidx.media3.common.util.UnstableApi
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
 import coil3.ImageLoader
 import coil3.PlatformContext
 import coil3.SingletonImageLoader
 import com.google.android.material.color.DynamicColors
 import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.guava.await
+import kotlinx.coroutines.launch
 import org.lineageos.twelve.database.TwelveDatabase
 import org.lineageos.twelve.repositories.MediaRepository
 import org.lineageos.twelve.repositories.ResumptionPlaylistRepository
+import org.lineageos.twelve.services.PlaybackService
 import org.lineageos.twelve.ui.coil.ThumbnailMapper
 
 @androidx.annotation.OptIn(UnstableApi::class)
@@ -23,11 +31,25 @@ class TwelveApplication : Application(), SingletonImageLoader.Factory {
     val mediaRepository by lazy { MediaRepository(applicationContext, MainScope(), database) }
     val resumptionPlaylistRepository by lazy { ResumptionPlaylistRepository(database) }
 
+    private val sessionToken by lazy {
+        SessionToken(
+            applicationContext,
+            ComponentName(applicationContext, PlaybackService::class.java)
+        )
+    }
+
+    private val _mediaControllerFlow = MutableSharedFlow<MediaController>(replay = 1)
+    val mediaControllerFlow = _mediaControllerFlow.asSharedFlow()
+
     override fun onCreate() {
         super.onCreate()
 
         // Observe dynamic colors changes
         DynamicColors.applyToActivitiesIfAvailable(this)
+
+        MainScope().launch {
+            initializeMediaController()
+        }
     }
 
     override fun newImageLoader(context: PlatformContext) = ImageLoader.Builder(this)
@@ -35,4 +57,12 @@ class TwelveApplication : Application(), SingletonImageLoader.Factory {
             add(ThumbnailMapper)
         }
         .build()
+
+    private suspend fun initializeMediaController() {
+        val mediaController = MediaController.Builder(this, sessionToken)
+            .buildAsync()
+            .await()
+
+        _mediaControllerFlow.emit(mediaController)
+    }
 }
