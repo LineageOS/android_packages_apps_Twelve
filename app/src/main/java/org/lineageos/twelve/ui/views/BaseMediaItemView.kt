9e3720c500ea387371bce3b4c7b1955504c756ac
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-02-09 23:32:38 +0100

    [WIP] Twelve: Merge all media item views
    Change-Id: I7fd4625d8a58602a1020c5bd9aefad4a202c08cc

---
+++ app/src/main/java/org/lineageos/twelve/ui/views/BaseMediaItemView.kt
@@ -0,0 +1,174 @@
+/*
+ * SPDX-FileCopyrightText: 2024-2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.twelve.ui.views
+
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.AttrRes
+import androidx.annotation.DrawableRes
+import androidx.annotation.LayoutRes
+import androidx.annotation.StringRes
+import androidx.core.view.isVisible
+import coil3.load
+import coil3.request.ImageRequest
+import com.google.android.material.card.MaterialCardView
+import org.lineageos.twelve.R
+import org.lineageos.twelve.models.Album
+import org.lineageos.twelve.models.Artist
+import org.lineageos.twelve.models.Audio
+import org.lineageos.twelve.models.Genre
+import org.lineageos.twelve.models.MediaItem
+import org.lineageos.twelve.models.Playlist
+
+abstract class BaseMediaItemView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    @AttrRes defStyleAttr: Int = com.google.android.material.R.attr.materialCardViewStyle,
+    @LayoutRes private val layoutResId: Int
+) : MaterialCardView(context, attrs, defStyleAttr) {
+    // Views
+    private val headlineTextView by lazy { findViewById<TextView>(R.id.headlineTextView) }
+    private val placeholderImageView by lazy { findViewById<ImageView>(R.id.placeholderImageView) }
+    private val subheadTextView by lazy { findViewById<TextView>(R.id.subheadTextView) }
+    private val supportingTextView by lazy { findViewById<TextView>(R.id.supportingTextView) }
+    private val thumbnailImageView by lazy { findViewById<ImageView>(R.id.thumbnailImageView) }
+
+    private var headlineText: CharSequence?
+        get() = headlineTextView.text
+        set(value) {
+            headlineTextView.setTextAndUpdateVisibility(value)
+        }
+
+    private var subheadText: CharSequence?
+        get() = subheadTextView.text
+        set(value) {
+            subheadTextView.setTextAndUpdateVisibility(value)
+        }
+
+    private var supportingText: CharSequence?
+        get() = supportingTextView.text
+        set(value) {
+            supportingTextView.setTextAndUpdateVisibility(value)
+        }
+
+    init {
+        setCardBackgroundColor(Color.TRANSPARENT)
+        cardElevation = 0f
+        strokeWidth = 0
+
+        inflate(context, layoutResId, this)
+    }
+
+    final override fun setCardBackgroundColor(color: Int) {
+        super.setCardBackgroundColor(color)
+    }
+
+    fun setItem(item: MediaItem<*>) {
+        when (item) {
+            is Album -> {
+                item.title?.let {
+                    headlineText = it
+                } ?: setHeadlineText(R.string.album_unknown)
+                subheadText = item.artistName
+                supportingText = item.year?.toString()
+
+                loadThumbnailImage(item.thumbnail, R.drawable.ic_album)
+            }
+
+            is Artist -> {
+                item.name?.let {
+                    headlineText = it
+                } ?: setHeadlineText(R.string.artist_unknown)
+                subheadText = null
+                supportingText = null
+
+                loadThumbnailImage(item.thumbnail, R.drawable.ic_person)
+            }
+
+            is Audio -> {
+                headlineText = item.title
+                subheadText = item.artistName
+                supportingText = item.albumTitle
+
+                setPlaceholderImage(R.drawable.ic_music_note)
+            }
+
+            is Genre -> {
+                item.name?.let {
+                    headlineText = it
+                } ?: setHeadlineText(R.string.genre_unknown)
+                subheadText = null
+                supportingText = null
+
+                setPlaceholderImage(R.drawable.ic_genres)
+            }
+
+            is Playlist -> {
+                headlineText = item.name
+                subheadText = null
+                supportingText = null
+
+                setPlaceholderImage(R.drawable.ic_playlist_play)
+            }
+        }
+    }
+
+    private fun setPlaceholderImage(@DrawableRes placeholder: Int) {
+        placeholderImageView.setImageResource(placeholder)
+        placeholderImageView.isVisible = true
+        thumbnailImageView.isVisible = false
+    }
+
+    private fun loadThumbnailImage(
+        data: Any?,
+        @DrawableRes placeholder: Int? = null,
+        builder: ImageRequest.Builder.() -> Unit = {
+            listener(
+                onCancel = {
+                    placeholder?.let {
+                        placeholderImageView.setImageResource(it)
+                        placeholderImageView.isVisible = true
+                    }
+                    thumbnailImageView.isVisible = false
+                },
+                onError = { _, _ ->
+                    placeholder?.let {
+                        placeholderImageView.setImageResource(it)
+                        placeholderImageView.isVisible = true
+                    }
+                    thumbnailImageView.isVisible = false
+                },
+                onSuccess = { _, _ ->
+                    placeholderImageView.isVisible = false
+                    thumbnailImageView.isVisible = true
+                },
+            )
+        }
+    ) = thumbnailImageView.load(data, builder = builder)
+
+    private fun setHeadlineText(@StringRes resId: Int) =
+        headlineTextView.setTextAndUpdateVisibility(resId)
+
+    private fun setSubheadText(@StringRes resId: Int) =
+        subheadTextView.setTextAndUpdateVisibility(resId)
+
+    private fun setSupportingText(@StringRes resId: Int) =
+        supportingTextView.setTextAndUpdateVisibility(resId)
+
+    // TextView utils
+
+    private fun TextView.setTextAndUpdateVisibility(text: CharSequence?) {
+        this.text = text.also {
+            isVisible = it != null
+        }
+    }
+
+    private fun TextView.setTextAndUpdateVisibility(@StringRes resId: Int) =
+        setTextAndUpdateVisibility(resources.getText(resId))
+}
--- app/src/main/java/org/lineageos/twelve/ui/views/MediaItemGridItem.kt
+++ app/src/main/java/org/lineageos/twelve/ui/views/MediaItemGridItem.kt
@@ -10,8 +10,8 @@ import android.util.AttributeSet
 import androidx.annotation.AttrRes
 import org.lineageos.twelve.R
 
-class ActivityTabItem @JvmOverloads constructor(
+class MediaItemGridItem @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     @AttrRes defStyleAttr: Int = com.google.android.material.R.attr.materialCardViewStyle,
-) : BaseMediaItemView(context, attrs, defStyleAttr, R.layout.item_activity_tab)
+) : BaseMediaItemView(context, attrs, defStyleAttr, R.layout.item_media_item_grid)
