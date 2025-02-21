b4b17b89feae23a8462eb7d78890be07dbc02845
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-02-21 10:30:02 +0100

    Twelve: MediaRepositoryTree: Don't crash in case of error
    Change-Id: Id03373a5e68f8b6c54041cbdc006c460cf1f4c72

--- app/src/main/java/org/lineageos/twelve/services/MediaRepositoryTree.kt
+++ app/src/main/java/org/lineageos/twelve/services/MediaRepositoryTree.kt
@@ -7,6 +7,7 @@ package org.lineageos.twelve.services
 
 import android.content.Context
 import android.net.Uri
+import android.util.Log
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MediaMetadata
 import kotlinx.coroutines.flow.Flow
@@ -321,6 +322,8 @@ class MediaRepositoryTree(
     }
 
     companion object {
+        private val LOG_TAG = MediaRepositoryTree::class.java.simpleName
+
         // Root ID
         private const val ROOT_MEDIA_ITEM_ID = "[root]"
 
@@ -343,7 +346,9 @@ class MediaRepositoryTree(
 
         /**
          * Converts a flow of [RequestStatus] to a one-shot result of [T].
-         * Raises an exception on error.
+         * On loading, returns null.
+         * On success, returns the data.
+         * On error, logs the exception and returns null.
          */
         private suspend fun <T, E> Flow<RequestStatus<T, E>>.toOneShotResult() = mapNotNull {
             when (it) {
@@ -355,9 +360,10 @@ class MediaRepositoryTree(
                     it.data
                 }
 
-                is RequestStatus.Error -> throw Exception(
-                    "Error while loading data, ${it.error}"
-                )
+                is RequestStatus.Error -> {
+                    Log.e(LOG_TAG, "Failed to get data", it.throwable)
+                    null
+                }
             }
         }.first()
     }
