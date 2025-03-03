691204d13093108a4d9e78f3689031f6c9e38452
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-03-03 17:51:52 +0100

    Twelve: Add playlist type handling
    Change-Id: Ie8b9b357c10c05f23be092479b3982005207a303

--- app/src/main/java/org/lineageos/twelve/fragments/AddOrRemoveFromPlaylistsFragment.kt
+++ app/src/main/java/org/lineageos/twelve/fragments/AddOrRemoveFromPlaylistsFragment.kt
@@ -90,8 +90,20 @@ class AddOrRemoveFromPlaylistsFragment : Fragment(R.layout.fragment_add_or_remov
                             }
                         }
 
-                        view.setLeadingIconImage(R.drawable.ic_playlist_play)
-                        view.headlineText = item.first.name
+                        view.setLeadingIconImage(
+                            when (item.first.type) {
+                                Playlist.Type.PLAYLIST -> R.drawable.ic_playlist_play
+                                Playlist.Type.FAVORITES -> R.drawable.ic_favorite
+                            }
+                        )
+                        item.first.name?.let {
+                            view.headlineText = it
+                        } ?: view.setHeadlineText(
+                            when (item.first.type) {
+                                Playlist.Type.PLAYLIST -> R.string.playlist_unknown
+                                Playlist.Type.FAVORITES -> R.string.favorites_playlist
+                            }
+                        )
                         view.setTrailingIconImage(
                             when (item.second) {
                                 true -> R.drawable.ic_check_circle
