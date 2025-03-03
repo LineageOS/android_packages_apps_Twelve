df05c7696696bcbbf89bf30582bf652410ee9f77
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-03-03 18:12:36 +0100

    Twelve: NowPlayingFragment: Wire up favorite
    Change-Id: Ia8252c63ac207daeb7928f670cc7070e2dda74eb

--- app/src/main/java/org/lineageos/twelve/fragments/NowPlayingFragment.kt
+++ app/src/main/java/org/lineageos/twelve/fragments/NowPlayingFragment.kt
@@ -77,6 +77,7 @@ class NowPlayingFragment : Fragment(R.layout.fragment_now_playing) {
     private val currentTimestampTextView by getViewProperty<TextView>(R.id.currentTimestampTextView)
     private val durationTimestampTextView by getViewProperty<TextView>(R.id.durationTimestampTextView)
     private val equalizerMaterialButton by getViewProperty<MaterialButton>(R.id.equalizerMaterialButton)
+    private val favoriteMaterialButton by getViewProperty<MaterialButton>(R.id.favoriteMaterialButton)
     private val fileTypeMaterialCardView by getViewProperty<MaterialCardView>(R.id.fileTypeMaterialCardView)
     private val fileTypeTextView by getViewProperty<TextView>(R.id.fileTypeTextView)
     private val linearProgressIndicator by getViewProperty<LinearProgressIndicator>(R.id.linearProgressIndicator)
@@ -533,6 +534,43 @@ class NowPlayingFragment : Fragment(R.layout.fragment_now_playing) {
                                 )
 
                                 lyricsMaterialCardView.isVisible = false
+                            }
+                        }
+                    }
+                }
+
+                launch {
+                    viewModel.favorite.collectLatest {
+                        when (it) {
+                            is RequestStatus.Loading -> {
+                                // Do nothing
+                            }
+
+                            is RequestStatus.Success -> {
+                                val isFavorite = it.data
+
+                                favoriteMaterialButton.setIconResource(
+                                    when (isFavorite) {
+                                        true -> R.drawable.ic_heart_filled
+                                        false -> R.drawable.ic_heart_unfilled
+                                    }
+                                )
+
+                                favoriteMaterialButton.setOnClickListener {
+                                    launch {
+                                        viewModel.setFavorite(!isFavorite)
+                                    }
+                                }
+                            }
+
+                            is RequestStatus.Error -> {
+                                Log.e(
+                                    LOG_TAG,
+                                    "Error while getting favorite status: ${it.error}",
+                                    it.throwable
+                                )
+
+                                favoriteMaterialButton.isVisible = false
                             }
                         }
                     }
