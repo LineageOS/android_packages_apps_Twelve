3ee503a9d59655193a3a66075f0336d829f0c116
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-03-10 17:37:36 +0100

    [WIP] Twelve: Implement SoundCloud provider
    Change-Id: I8e465f38edf7daf41c9a2b9d466a1f90c512efdf

---
+++ app/src/main/java/org/lineageos/twelve/datasources/soundcloud/SoundCloudAuthInterceptor.kt
@@ -0,0 +1,36 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.twelve.datasources.soundcloud
+
+import okhttp3.Interceptor
+
+class SoundCloudAuthInterceptor(
+    private val clientId: String,
+    private val oAuthToken: String?,
+) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain) = chain.proceed(
+        chain.request().newBuilder()
+            .url(
+                chain.request().url.newBuilder()
+                    .apply {
+                        getAuthParameters().forEach { (key, value) ->
+                            addQueryParameter(key, value)
+                        }
+                    }
+                    .build()
+            )
+            .apply {
+                oAuthToken?.let {
+                    addHeader("Authorization", "OAuth $it")
+                }
+            }
+            .build()
+    )
+
+    fun getAuthParameters() = listOf(
+        "client_id" to clientId,
+    )
+}
