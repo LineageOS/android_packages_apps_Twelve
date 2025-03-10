5514e47fd468b5567e94e68c9d2565d10439af8d
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-03-10 17:37:36 +0100

    [WIP] Twelve: Implement SoundCloud provider
    Change-Id: I8e465f38edf7daf41c9a2b9d466a1f90c512efdf

---
+++ app/src/main/java/org/lineageos/twelve/datasources/soundcloud/models/PartialTrack.kt
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.twelve.datasources.soundcloud.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Suppress("PROVIDED_RUNTIME_TOO_LOW")
+@Serializable
+data class PartialTrack(
+    val id: Int,
+    val kind: String,
+    @SerialName("monetization_model") val monetizationModel: String,
+    val policy: String,
+)
