67bd606ef0689e0c73d601b8ea2615170f69a2ad
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-03-10 17:37:36 +0100

    [WIP] Twelve: Implement SoundCloud provider
    Change-Id: I8e465f38edf7daf41c9a2b9d466a1f90c512efdf

---
+++ app/src/main/java/org/lineageos/twelve/datasources/SoundCloudDataSource.kt
@@ -0,0 +1,474 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.twelve.datasources
+
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import androidx.media3.common.MimeTypes
+import androidx.media3.common.util.UnstableApi
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flowOf
+import okhttp3.Cache
+import org.lineageos.twelve.R
+import org.lineageos.twelve.datasources.soundcloud.SoundCloudClient
+import org.lineageos.twelve.datasources.soundcloud.models.Selection
+import org.lineageos.twelve.datasources.soundcloud.models.SystemPlaylist
+import org.lineageos.twelve.datasources.soundcloud.models.Track
+import org.lineageos.twelve.datasources.soundcloud.models.User
+import org.lineageos.twelve.ext.mapAsync
+import org.lineageos.twelve.models.ActivityTab
+import org.lineageos.twelve.models.Album
+import org.lineageos.twelve.models.Artist
+import org.lineageos.twelve.models.ArtistWorks
+import org.lineageos.twelve.models.Audio
+import org.lineageos.twelve.models.DataSourceInformation
+import org.lineageos.twelve.models.Error
+import org.lineageos.twelve.models.Genre
+import org.lineageos.twelve.models.GenreContent
+import org.lineageos.twelve.models.LocalizedString
+import org.lineageos.twelve.models.Lyrics
+import org.lineageos.twelve.models.MediaItem
+import org.lineageos.twelve.models.MediaType
+import org.lineageos.twelve.models.Playlist
+import org.lineageos.twelve.models.ProviderArgument
+import org.lineageos.twelve.models.ProviderArgument.Companion.getArgument
+import org.lineageos.twelve.models.ProviderArgument.Companion.requireArgument
+import org.lineageos.twelve.models.Result
+import org.lineageos.twelve.models.Result.Companion.getOrNull
+import org.lineageos.twelve.models.Result.Companion.map
+import org.lineageos.twelve.models.SortingRule
+import org.lineageos.twelve.models.Thumbnail
+
+/**
+ * SoundCloud backed data source.
+ */
+class SoundCloudDataSource(
+    arguments: Bundle,
+    cache: Cache? = null,
+) : MediaDataSource {
+    private val clientId = arguments.requireArgument(ARG_CLIENT_ID)
+    private val oAuthToken = arguments.getArgument(ARG_OAUTH_TOKEN)
+
+    private val soundCloudClient = SoundCloudClient(clientId, oAuthToken, cache)
+
+    private val albumsUri = SoundCloudClient.serverUri.buildUpon()
+        .appendPath(ALBUMS_PATH)
+        .build()
+    private val artistsUri = SoundCloudClient.serverUri.buildUpon()
+        .appendPath(ARTISTS_PATH)
+        .build()
+    private val audiosUri = SoundCloudClient.serverUri.buildUpon()
+        .appendPath(AUDIOS_PATH)
+        .build()
+    private val genresUri = SoundCloudClient.serverUri.buildUpon()
+        .appendPath(GENRES_PATH)
+        .build()
+    private val playlistsUri = SoundCloudClient.serverUri.buildUpon()
+        .appendPath(PLAYLISTS_PATH)
+        .build()
+    private val systemPlaylistsUri = SoundCloudClient.serverUri.buildUpon()
+        .appendPath(SYSTEM_PLAYLISTS_PATH)
+        .build()
+
+    private val favoritesUri = SoundCloudClient.serverUri.buildUpon()
+        .appendPath(FAVORITES_PATH)
+        .build()
+    private val favoritesPlaylist = Playlist.Builder(favoritesUri)
+        .setType(Playlist.Type.FAVORITES)
+        .build()
+
+    override fun status() = suspend {
+        val me = soundCloudClient.me()
+
+        me.map { user ->
+            listOfNotNull(
+                DataSourceInformation(
+                    "username",
+                    LocalizedString.StringResIdLocalizedString(R.string.soundcloud_username),
+                    LocalizedString.StringLocalizedString(user.username),
+                ),
+                DataSourceInformation(
+                    "full_name",
+                    LocalizedString.StringResIdLocalizedString(R.string.soundcloud_full_name),
+                    LocalizedString.StringLocalizedString(user.fullName),
+                ),
+                user.primaryEmail?.let {
+                    DataSourceInformation(
+                        "primary_email",
+                        LocalizedString.StringResIdLocalizedString(
+                            R.string.soundcloud_primary_email
+                        ),
+                        LocalizedString.StringLocalizedString(it),
+                    )
+                },
+                user.primaryEmailConfirmed?.let {
+                    DataSourceInformation(
+                        "primary_email_confirmed",
+                        LocalizedString.StringResIdLocalizedString(
+                            R.string.soundcloud_primary_email_confirmed
+                        ),
+                        LocalizedString.of(it),
+                    )
+                },
+                user.city?.let {
+                    DataSourceInformation(
+                        "city",
+                        LocalizedString.StringResIdLocalizedString(R.string.soundcloud_city),
+                        LocalizedString.StringLocalizedString(it),
+                    )
+                },
+                user.dateOfBirth?.let {
+                    DataSourceInformation(
+                        "date_of_birth",
+                        LocalizedString.StringResIdLocalizedString(
+                            R.string.soundcloud_date_of_birth
+                        ),
+                        LocalizedString.StringResIdLocalizedString(
+                            R.string.soundcloud_date_of_birth_format,
+                            listOf(it.year, it.month),
+                        ),
+                    )
+                },
+                user.downloadsDisabled?.let {
+                    DataSourceInformation(
+                        "downloads_disabled",
+                        LocalizedString.StringResIdLocalizedString(
+                            R.string.soundcloud_downloads_disabled
+                        ),
+                        LocalizedString.of(it),
+                    )
+                },
+            )
+        }
+    }.asFlow()
+
+    override suspend fun mediaTypeOf(mediaItemUri: Uri) = with(mediaItemUri.toString()) {
+        when {
+            startsWith(albumsUri.toString()) -> MediaType.ALBUM
+            startsWith(artistsUri.toString()) -> MediaType.ARTIST
+            startsWith(audiosUri.toString()) -> MediaType.AUDIO
+            startsWith(genresUri.toString()) -> MediaType.GENRE
+            startsWith(playlistsUri.toString()) -> MediaType.PLAYLIST
+            startsWith(systemPlaylistsUri.toString()) -> MediaType.PLAYLIST
+            mediaItemUri == favoritesUri -> MediaType.PLAYLIST
+            else -> null
+        }
+    }
+
+    override fun activity() = suspend {
+        soundCloudClient.getMixedSelections().map {
+            it.collection.map { selection -> selection.toModel() }
+        }
+    }.asFlow()
+
+    override fun albums(sortingRule: SortingRule) = flowOf(
+        Result.Success<_, Error>(listOf<Album>())
+    )
+
+    override fun artists(sortingRule: SortingRule) = flowOf(
+        Result.Success<_, Error>(listOf<Artist>())
+    )
+
+    override fun genres(sortingRule: SortingRule) = flowOf(
+        Result.Success<_, Error>(listOf<Genre>())
+    )
+
+    override fun playlists(sortingRule: SortingRule) = flowOf(
+        Result.Success<_, Error>(listOf(favoritesPlaylist))
+    )
+
+    override fun search(query: String) = flowOf(
+        Result.Success<_, Error>(listOf<MediaItem<*>>())
+    )
+
+    override fun audio(audioUri: Uri) = suspend {
+        soundCloudClient.getTrack(
+            audioUri.lastPathSegment!!.toInt(),
+            audioUri.getQueryParameter("secret_token")
+        ).map { it.toModel() }
+    }.asFlow()
+
+    override fun album(albumUri: Uri) = suspend {
+        val albumId = albumUri.lastPathSegment!!.toInt()
+
+        soundCloudClient.getPlaylist(albumId).map {
+            it.toAlbum() to resolveTracks(it.tracks.map { track -> track.id }).orEmpty()
+        }
+    }.asFlow()
+
+    override fun artist(artistUri: Uri) = suspend {
+        val artistId = artistUri.lastPathSegment!!.toInt()
+
+        soundCloudClient.getUser(artistId).map {
+            it.toModel() to ArtistWorks(
+                albums = soundCloudClient.getUserAlbums(
+                    artistId
+                ).getOrNull()?.collection?.map { set -> set.toAlbum() }.orEmpty(),
+                appearsInAlbum = listOf(),
+                appearsInPlaylist = listOf(),
+            )
+        }
+    }.asFlow()
+
+    override fun genre(genreUri: Uri) = flowOf(
+        Result.Error<Pair<Genre, GenreContent>, _>(Error.NOT_FOUND)
+    )
+
+    override fun playlist(playlistUri: Uri) = suspend {
+        when {
+            playlistUri == favoritesUri -> soundCloudClient.getLikedTracks().map {
+                favoritesPlaylist to resolveTracks(it.collection).orEmpty()
+            }
+
+            else -> {
+                val playlistId = playlistUri.lastPathSegment!!.toInt()
+
+                soundCloudClient.getPlaylist(
+                    playlistId,
+                    secretToken = playlistUri.getQueryParameter("secret_token"),
+                ).map {
+                    it.toPlaylist() to resolveTracks(
+                        it.tracks.map { track -> track.id }
+                    ).orEmpty()
+                }
+            }
+        }
+    }.asFlow()
+
+    override fun audioPlaylistsStatus(audioUri: Uri) = flowOf(
+        Result.Error<List<Pair<Playlist, Boolean>>, _>(Error.NOT_FOUND)
+    )
+
+    override fun lyrics(audioUri: Uri) = flowOf(
+        Result.Error<Lyrics, _>(Error.NOT_FOUND)
+    )
+
+    override suspend fun createPlaylist(name: String) =
+        Result.Error<Uri, _>(Error.NOT_IMPLEMENTED)
+
+    override suspend fun renamePlaylist(playlistUri: Uri, name: String) = when {
+        playlistUri == favoritesUri -> Result.Error(Error.IO)
+        else -> Result.Error<Unit, _>(Error.NOT_IMPLEMENTED)
+    }
+
+    override suspend fun deletePlaylist(playlistUri: Uri) = when {
+        playlistUri == favoritesUri -> Result.Error(Error.IO)
+        else -> Result.Error<Unit, _>(Error.NOT_IMPLEMENTED)
+    }
+
+    override suspend fun addAudioToPlaylist(
+        playlistUri: Uri,
+        audioUri: Uri
+    ) = when {
+        playlistUri == favoritesUri -> setFavorite(audioUri, true)
+        else -> Result.Error(Error.NOT_IMPLEMENTED)
+    }
+
+    override suspend fun removeAudioFromPlaylist(
+        playlistUri: Uri,
+        audioUri: Uri
+    ) = when {
+        playlistUri == favoritesUri -> setFavorite(audioUri, false)
+        else -> Result.Error(Error.NOT_IMPLEMENTED)
+    }
+
+    override suspend fun onAudioPlayed(audioUri: Uri) = Result.Success<_, Error>(Unit)
+
+    override suspend fun setFavorite(
+        audioUri: Uri,
+        isFavorite: Boolean
+    ) = audioUri.lastPathSegment!!.toInt().let {
+        when (isFavorite) {
+            true -> soundCloudClient.likeTrack(it)
+            else -> soundCloudClient.unlikeTrack(it)
+        }
+    }
+
+    private suspend fun resolveTracks(
+        trackIds: List<Int>
+    ) = trackIds.chunked(20).mapAsync { ids ->
+        soundCloudClient.getTracks(ids).let {
+            when (it) {
+                is Result.Success -> it.data.sortedBy { track ->
+                    trackIds.indexOf(track.id)
+                }.mapAsync { track -> track.toModel() }
+
+                is Result.Error -> {
+                    Log.i(LOG_TAG, "Failed to fetch tracks: ${it.error}", it.throwable)
+                    null
+                }
+            }
+        }
+    }.let {
+        if (it.any { tracks -> tracks == null }) {
+            null
+        } else {
+            it.filterNotNull().flatten()
+        }
+    }
+
+    private fun org.lineageos.twelve.datasources.soundcloud.models.Playlist.toAlbum() =
+        Album.Builder(getAlbumUri(id))
+            .setThumbnail(
+                Thumbnail.Builder()
+                    .setUri(artworkUrl)
+                    .build()
+            )
+            .setTitle(title)
+            .setArtistUri(getArtistUri(user.id))
+            .setArtistName(
+                listOfNotNull(
+                    user.fullName,
+                    user.username,
+                ).firstOrNull { it.isNotEmpty() }
+            )
+            .setYear(null) // TODO
+            .build()
+
+    private fun org.lineageos.twelve.datasources.soundcloud.models.Playlist.toPlaylist() =
+        Playlist.Builder(getPlaylistUri(id))
+            .setThumbnail(
+                Thumbnail.Builder()
+                    .setUri(artworkUrl)
+                    .build()
+            )
+            .setName(title)
+            .setType(Playlist.Type.PLAYLIST)
+            .build()
+
+    private fun Selection.toModel() = ActivityTab(
+        id,
+        LocalizedString.StringLocalizedString(title),
+        items.collection.map {
+            when (it) {
+                is org.lineageos.twelve.datasources.soundcloud.models.Playlist -> it.toPlaylist()
+                is SystemPlaylist -> it.toModel()
+                else -> error("Unsupported selection type")
+            }
+        }
+    )
+
+    private fun SystemPlaylist.toModel() = Playlist.Builder(getSystemPlaylistUri(id))
+        .setThumbnail(
+            Thumbnail.Builder()
+                .setUri(artworkUrl)
+                .build()
+        )
+        .setName(title)
+        .setType(Playlist.Type.PLAYLIST) // TODO
+        .build()
+
+    private suspend fun Track.toModel() = Audio.Builder(getAudioUri(id))
+        .setThumbnail(
+            Thumbnail.Builder()
+                .setUri(artworkUrl)
+                .build()
+        )
+        .apply {
+            @androidx.annotation.OptIn(UnstableApi::class)
+            val transcoding = media.transcodings.firstOrNull {
+                !it.snipped
+                        && MimeTypes.isAudio(it.format.mimeType)
+                        && !it.format.mimeType.startsWith(MimeTypes.AUDIO_MP4)
+                        && !it.format.protocol.contains("-encrypted-")
+            }
+
+            val directUrl = transcoding?.let {
+                soundCloudClient.getTrackTranscodingDirectUrl(it.url).getOrNull()
+            }
+
+            setPlaybackUri(directUrl)
+            //setMimeType(transcoding?.format?.mimeType) // TODO: Doesn't like audio/mpeg
+        }
+        .setTitle(title)
+        .setType(Audio.Type.MUSIC)
+        .setDurationMs(fullDuration)
+        .setArtistUri(userId?.let { getArtistUri(it) })
+        .setArtistName(
+            listOfNotNull(
+                publisherMetadata?.artist,
+                user.fullName,
+                user.username,
+            ).firstOrNull { it.isNotEmpty() }
+        )
+        .setAlbumUri(null)
+        .setAlbumTitle(publisherMetadata?.albumTitle)
+        .setDiscNumber(null)
+        .setTrackNumber(null)
+        .setGenreUri(null)
+        .setGenreName(genre)
+        .setYear(null)
+        .setIsFavorite(false)
+        .build()
+
+    private fun User.toModel() = Artist.Builder(getArtistUri(id))
+        .setThumbnail(
+            Thumbnail.Builder()
+                .setUri(avatarUrl)
+                .build()
+        )
+        .setName(
+            listOfNotNull(
+                fullName,
+                username,
+            ).firstOrNull { it.isNotEmpty() }
+        )
+        .build()
+
+    private fun getAlbumUri(albumId: Int) = albumsUri.buildUpon()
+        .appendPath("$albumId")
+        .build()
+
+    private fun getArtistUri(artistId: Int) = artistsUri.buildUpon()
+        .appendPath("$artistId")
+        .build()
+
+    private fun getAudioUri(audioId: Int) = audiosUri.buildUpon()
+        .appendPath("$audioId")
+        .build()
+
+    private fun getGenreUri(genre: Int) = genresUri.buildUpon()
+        .appendPath("$genre")
+        .build()
+
+    private fun getPlaylistUri(playlistId: Int) = playlistsUri.buildUpon()
+        .appendPath("$playlistId")
+        .build()
+
+    private fun getSystemPlaylistUri(systemPlaylistId: String) = systemPlaylistsUri.buildUpon()
+        .appendPath(systemPlaylistId)
+        .build()
+
+    companion object {
+        private val LOG_TAG = SoundCloudDataSource::class.simpleName!!
+
+        private const val ALBUMS_PATH = "albums"
+        private const val ARTISTS_PATH = "artists"
+        private const val AUDIOS_PATH = "audio"
+        private const val GENRES_PATH = "genres"
+        private const val PLAYLISTS_PATH = "playlists"
+        private const val SYSTEM_PLAYLISTS_PATH = "system_playlists"
+
+        private const val FAVORITES_PATH = "favorites"
+
+        val ARG_CLIENT_ID = ProviderArgument(
+            "client_id",
+            String::class,
+            R.string.provider_argument_client_id,
+            required = true,
+            hidden = true,
+        )
+
+        val ARG_OAUTH_TOKEN = ProviderArgument(
+            "oauth_token",
+            String::class,
+            R.string.provider_argument_oauth_token,
+            required = false,
+            hidden = true,
+        )
+    }
+}
---
+++ app/src/main/java/org/lineageos/twelve/datasources/soundcloud/SoundCloudClient.kt
@@ -0,0 +1,326 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.twelve.datasources.soundcloud
+
+import android.net.Uri
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import org.lineageos.twelve.datasources.soundcloud.models.Access
+import org.lineageos.twelve.datasources.soundcloud.models.PaginatedCollection
+import org.lineageos.twelve.datasources.soundcloud.models.Selection
+import org.lineageos.twelve.datasources.soundcloud.models.Streams
+import org.lineageos.twelve.datasources.soundcloud.models.Track
+import org.lineageos.twelve.datasources.soundcloud.models.TranscodingDirectUrl
+import org.lineageos.twelve.datasources.soundcloud.models.User
+import org.lineageos.twelve.models.Result.Companion.map
+import org.lineageos.twelve.utils.Api
+import org.lineageos.twelve.utils.ApiRequest
+import org.lineageos.twelve.utils.mapToError
+import org.lineageos.twelve.datasources.soundcloud.models.Playlist as SoundCloudSet
+
+/**
+ * SoundCloud client.
+ *
+ * [API reference](https://developers.soundcloud.com/docs/api/explorer/open-api)
+ *
+ * @param cache OkHttp's [Cache]
+ */
+class SoundCloudClient(
+    clientId: String,
+    oAuthToken: String? = null,
+    private val cache: Cache? = null,
+) {
+    private val authInterceptor = SoundCloudAuthInterceptor(clientId, oAuthToken)
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(authInterceptor)
+        .cache(cache)
+        .build()
+
+    private val api = Api(okHttpClient, serverUri)
+
+    /**
+     * Returns the authenticated user's information.
+     */
+    suspend fun me() = ApiRequest.get<User>(
+        listOf("me")
+    ).execute(api).mapToError()
+
+    suspend fun getLikedTracks(
+        limit: Int = 50,
+        access: Set<Access> = setOf(Access.PLAYABLE, Access.PREVIEW, Access.BLOCKED),
+        linkedPartitioning: Boolean = true,
+    ) = ApiRequest.get<PaginatedCollection<Int>>(
+        listOf(
+            "me",
+            "track_likes",
+            "ids",
+        ),
+        queryParameters = listOf(
+            "limit" to limit,
+            "access_token" to access.joinToString(",") { it.value },
+            "linked_partitioning" to when (linkedPartitioning) {
+                true -> "1"
+                false -> "0"
+            },
+        )
+    ).execute(api).mapToError()
+
+    suspend fun getLikedPlaylists(
+        limit: Int = 50,
+        linkedPartitioning: Boolean = true,
+    ) = ApiRequest.get<PaginatedCollection<SoundCloudSet>>(
+        listOf(
+            "me",
+            "likes",
+            "playlists",
+        ),
+        queryParameters = listOf(
+            "limit" to limit,
+            "linked_partitioning" to when (linkedPartitioning) {
+                true -> "1"
+                false -> "0"
+            },
+        )
+    ).execute(api).mapToError()
+
+    suspend fun getUserPlaylists(
+        showTracks: Boolean = true,
+        linkedPartitioning: Boolean = true,
+        limit: Int = 50,
+    ) = ApiRequest.get<PaginatedCollection<SoundCloudSet>>(
+        listOf(
+            "me",
+            "playlists",
+        ),
+        queryParameters = listOf(
+            "show_tracks" to showTracks,
+            "linked_partitioning" to when (linkedPartitioning) {
+                true -> "1"
+                false -> "0"
+            },
+            "limit" to limit,
+        )
+    ).execute(api).mapToError()
+
+    suspend fun getUserTracks(
+        limit: Int = 50,
+        linkedPartitioning: Boolean = true,
+    ) = ApiRequest.get<PaginatedCollection<Track>>(
+        listOf(
+            "me",
+            "tracks",
+        ),
+        queryParameters = listOf(
+            "limit" to limit,
+            "linked_partitioning" to when (linkedPartitioning) {
+                true -> "1"
+                false -> "0"
+            },
+        )
+    ).execute(api).mapToError()
+
+    suspend fun getMixedSelections(
+        limit: Int = 10,
+        offset: Int = 0,
+        linkedPartitioning: Boolean = true,
+    ) = ApiRequest.get<PaginatedCollection<Selection>>(
+        listOf(
+            "mixed-selections",
+        ),
+        queryParameters = listOf(
+            "limit" to limit,
+            "offset" to offset,
+            "linked_partitioning" to when (linkedPartitioning) {
+                true -> "1"
+                false -> "0"
+            },
+        )
+    ).execute(api).mapToError()
+
+    suspend fun getPlaylist(
+        playlistId: Int,
+        secretToken: String? = null,
+        access: Set<Access> = setOf(Access.PLAYABLE, Access.PREVIEW),
+        showTracks: Boolean = true,
+    ) = ApiRequest.get<SoundCloudSet>(
+        listOf(
+            "playlists",
+            "$playlistId",
+        ),
+        queryParameters = listOf(
+            "secret_token" to secretToken,
+            "access_token" to access.joinToString(",") { it.value },
+            "show_tracks" to showTracks,
+        )
+    ).execute(api).mapToError()
+
+    suspend fun deletePlaylist(
+        playlistId: Int,
+    ) = ApiRequest.delete<Unit>(
+        listOf(
+            "playlists",
+            "$playlistId",
+        ),
+    ).execute(api).mapToError()
+
+    suspend fun getTracks(
+        trackIds: List<Int>,
+    ) = ApiRequest.get<List<Track>>(
+        listOf(
+            "tracks",
+        ),
+        queryParameters = listOf(
+            "ids" to trackIds.joinToString(",")
+        )
+    ).execute(api).mapToError()
+
+    suspend fun getTrack(
+        trackId: Int,
+        secretToken: String? = null,
+    ) = ApiRequest.get<Track>(
+        listOf(
+            "tracks",
+            "$trackId",
+        ),
+        queryParameters = listOf(
+            "secret_token" to secretToken,
+        )
+    ).execute(api).mapToError()
+
+    suspend fun getTrackTranscodingDirectUrl(
+        url: Uri,
+    ) = ApiRequest.get<TranscodingDirectUrl>(
+        url.pathSegments
+    ).execute(api).mapToError().map {
+        addAuthParameters(it.url)
+    }
+
+    suspend fun getTrackStreams(
+        trackId: Int,
+        secretToken: String? = null,
+    ) = ApiRequest.get<Streams>(
+        listOf(
+            "tracks",
+            "$trackId",
+            "streams",
+        ),
+        queryParameters = listOf(
+            "secret_token" to secretToken,
+        )
+    ).execute(api).mapToError()
+
+    suspend fun getUser(
+        userId: Int,
+    ) = ApiRequest.get<User>(
+        listOf(
+            "users",
+            "$userId",
+        ),
+    ).execute(api).mapToError()
+
+    suspend fun getUserAlbums(
+        userId: Int,
+        limit: Int = 50,
+        offset: Int = 0,
+        linkedPartitioning: Boolean = true,
+    ) = ApiRequest.get<PaginatedCollection<SoundCloudSet>>(
+        listOf(
+            "users",
+            "$userId",
+            "albums",
+        ),
+        queryParameters = listOf(
+            "limit" to limit,
+            "offset" to offset,
+            "linked_partitioning" to when (linkedPartitioning) {
+                true -> "1"
+                false -> "0"
+            },
+        )
+    ).execute(api).mapToError()
+
+    suspend fun getUserPlaylists(
+        userId: Int,
+        access: Set<Access> = setOf(Access.PLAYABLE, Access.PREVIEW),
+        showTracks: Boolean = true,
+        limit: Int = 50,
+        linkedPartitioning: Boolean = true,
+    ) = ApiRequest.get<PaginatedCollection<SoundCloudSet>>(
+        listOf(
+            "users",
+            "$userId",
+            "playlists",
+        ),
+        queryParameters = listOf(
+            "access_token" to access.joinToString(",") { it.value },
+            "show_tracks" to showTracks,
+            "limit" to limit,
+            "linked_partitioning" to when (linkedPartitioning) {
+                true -> "1"
+                false -> "0"
+            },
+        )
+    ).execute(api).mapToError()
+
+    suspend fun getUserTracks(
+        userId: Int,
+        access: Set<Access> = setOf(Access.PLAYABLE, Access.PREVIEW),
+        limit: Int = 50,
+        linkedPartitioning: Boolean = true,
+    ) = ApiRequest.get<PaginatedCollection<Track>>(
+        listOf(
+            "users",
+            "$userId",
+            "tracks",
+        ),
+        queryParameters = listOf(
+            "access_token" to access.joinToString(",") { it.value },
+            "limit" to limit,
+            "linked_partitioning" to when (linkedPartitioning) {
+                true -> "1"
+                false -> "0"
+            },
+        )
+    ).execute(api).mapToError()
+
+    suspend fun likeTrack(
+        trackId: Int,
+    ) = ApiRequest.post<Any, Unit>(
+        listOf(
+            "likes",
+            "tracks",
+            "$trackId",
+        ),
+    ).execute(api).mapToError()
+
+    suspend fun unlikeTrack(
+        trackId: Int,
+    ) = ApiRequest.delete<Unit>(
+        listOf(
+            "likes",
+            "tracks",
+            "$trackId",
+        ),
+    ).execute(api).mapToError()
+
+    fun addAuthParameters(uri: Uri): Uri = uri.buildUpon()
+        .apply {
+            authInterceptor.getAuthParameters().forEach { (key, value) ->
+                appendQueryParameter(key, value)
+            }
+        }
+        .build()
+
+    companion object {
+        private const val AUTHORITY = "api-v2.soundcloud.com"
+
+        val serverUri: Uri = Uri.Builder()
+            .scheme("https")
+            .authority(AUTHORITY)
+            .build()
+    }
+}
