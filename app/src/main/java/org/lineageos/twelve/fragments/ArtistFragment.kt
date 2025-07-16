d308ea765a53a43ea6ce804824f2aaf61ee75026
Timi <timi.rautamaki@gmail.com>
2025-07-16 20:05:24 +0300

    Twelve: Add artist shuffle play
    Change-Id: Ia50205bc1f252948ba62d351d057e0f78523f3c3

--- app/src/main/java/org/lineageos/twelve/fragments/ArtistFragment.kt
+++ app/src/main/java/org/lineageos/twelve/fragments/ArtistFragment.kt
@@ -5,10 +5,12 @@
 
 package org.lineageos.twelve.fragments
 
+import android.animation.ValueAnimator
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.animation.DecelerateInterpolator
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -26,6 +28,7 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.google.android.material.progressindicator.LinearProgressIndicator
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -38,6 +41,7 @@ import org.lineageos.twelve.ext.navigateSafe
 import org.lineageos.twelve.ext.setProgressCompat
 import org.lineageos.twelve.ext.updatePadding
 import org.lineageos.twelve.models.Album
+import org.lineageos.twelve.models.Audio
 import org.lineageos.twelve.models.Error
 import org.lineageos.twelve.models.FlowResult
 import org.lineageos.twelve.models.Playlist
@@ -69,6 +73,9 @@ class ArtistFragment : CollapsingToolbarLayoutFragment(R.layout.fragment_artist)
     private val linearProgressIndicator by getViewProperty<LinearProgressIndicator>(R.id.linearProgressIndicator)
     private val nestedScrollView by getViewProperty<NestedScrollView>(R.id.nestedScrollView)
     private val noElementsNestedScrollView by getViewProperty<NestedScrollView>(R.id.noElementsNestedScrollView)
+    private val playArtistTracksExtendedFloatingActionButton by getViewProperty<ExtendedFloatingActionButton>(
+        R.id.playArtistTracksExtendedFloatingActionButton
+    )
     private val thumbnailImageView by getViewProperty<ImageView>(R.id.thumbnailImageView)
     private val toolbar by getViewProperty<MaterialToolbar>(R.id.toolbar)
 
@@ -185,6 +192,37 @@ class ArtistFragment : CollapsingToolbarLayoutFragment(R.layout.fragment_artist)
             )
 
             windowInsets
+        }
+
+        playArtistTracksExtendedFloatingActionButton.setOnClickListener {
+            lifecycleScope.launch {
+                viewModel.artistTracks.collectLatest { requestStatus ->
+                    if (requestStatus is FlowResult.Success) {
+                        val tracks = requestStatus.data.items.filterIsInstance<Audio>()
+                        viewModel.playAudio(tracks.shuffled(), 0)
+                    }
+                }
+            }
+        }
+
+        // Adjust FAB bottom margin to be above the NowPlayingBar
+        val nowPlayingBar = requireActivity().findViewById<View>(R.id.nowPlayingBar)
+        val fabMarginBottom = resources.getDimensionPixelSize(R.dimen.fab_margin_bottom)
+
+        nowPlayingBar?.addOnLayoutChangeListener { v, _, _, _, _, _, _, _, _ ->
+            val fab = playArtistTracksExtendedFloatingActionButton
+            val params = fab.layoutParams as CoordinatorLayout.LayoutParams
+            val targetMargin = if (v.isVisible) v.height + fabMarginBottom else fabMarginBottom
+
+            ValueAnimator.ofInt(params.bottomMargin, targetMargin).apply {
+                duration = 200
+                interpolator = DecelerateInterpolator()
+                addUpdateListener {
+                    params.bottomMargin = it.animatedValue as Int
+                    fab.layoutParams = params
+                }
+                start()
+            }
         }
 
         toolbar.setupWithNavController(findNavController())
