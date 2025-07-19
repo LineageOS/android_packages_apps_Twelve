87e9efbce9671c3bfb225fee62b5f1345166c7c8
Timi <timi.rautamaki@gmail.com>
2025-07-19 15:10:30 +0300

    Twelve: Create instant mix once queue is empty
    Change-Id: I5673520a6b2bd2f8b7c0a206777932534444092f

--- app/src/main/java/org/lineageos/twelve/datasources/JellyfinDataSource.kt
+++ app/src/main/java/org/lineageos/twelve/datasources/JellyfinDataSource.kt
@@ -12,6 +12,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.mapLatest
 import okhttp3.Cache
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
@@ -621,6 +622,7 @@ class JellyfinDataSource(
         }
     }
 
+
     override suspend fun onAudioPlayed(
         audioUri: Uri,
         positionMs: Long,
@@ -630,6 +632,26 @@ class JellyfinDataSource(
             itemId = itemId,
             positionTicks = positionMs,
         )
+    }
+
+    override fun getSuggestionsFromAudio(
+        providerIdentifier: ProviderIdentifier,
+        audioUri: Uri,
+    ) = providersManager.flatMapWithInstanceOf(providerIdentifier) {
+        flow {
+            val id = UUID.fromString(audioUri.lastPathSegment!!)
+            val result = client.songInstantMix(id).map { queryResult ->
+                val tracks =
+                    queryResult.items.map { it.toMediaItemAudio() }.filter { it.uri != audioUri }
+
+                ActivityTab(
+                    "instant mix",
+                    LocalizedString.StringLocalizedString("Instant mix"),
+                    tracks
+                )
+            }
+            emit(result)
+        }
     }
 
     override suspend fun setFavorite(
--- app/src/main/java/org/lineageos/twelve/datasources/jellyfin/JellyfinClient.kt
+++ app/src/main/java/org/lineageos/twelve/datasources/jellyfin/JellyfinClient.kt
@@ -332,6 +332,17 @@ class JellyfinClient(
             "Playing",
         ),
         data = PlaybackRequest(itemId.toString(), positionTicks)
+    )
+
+    suspend fun songInstantMix(id: UUID) = ApiRequest.get<QueryResult>(
+        listOf(
+            "Songs",
+            id.toString(),
+            "InstantMix",
+        ),
+        queryParameters = listOf(
+            "Limit" to 20,
+        )
     ).execute(api).mapToError()
 
     private suspend fun getItem(id: UUID) = ApiRequest.get<Item>(
