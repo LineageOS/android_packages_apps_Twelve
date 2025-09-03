e9af2b615aa649964d937d72d78c7fa2555360fc
Inhishonor <inhishonor@protonmail.com>
2025-09-03 10:13:23 -0700

    Twelve: Play all songs
    Change-Id: Ic96be3656a230511a22380262cdc097148c5d676

--- app/src/main/java/org/lineageos/twelve/datasources/JellyfinDataSource.kt
+++ app/src/main/java/org/lineageos/twelve/datasources/JellyfinDataSource.kt
@@ -12,6 +12,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.mapLatest
 import okhttp3.Cache
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
@@ -351,6 +352,14 @@ class JellyfinDataSource(
                 }
             }
         )
+    }
+
+    override fun allSongs(
+        providerIdentifier: ProviderIdentifier
+    ) = providersManager.mapWithInstanceOf(providerIdentifier) {
+        client.getAllSongs().map { queryResult ->
+                queryResult.items.map { it.toMediaItemAudio() }
+        }
     }
 
     override fun albums(
