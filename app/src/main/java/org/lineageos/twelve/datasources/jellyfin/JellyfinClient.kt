d7903700185fe41c8942fd48a6cac60f8f7ad688
Timi <timi.rautamaki@gmail.com>
2026-01-23 10:34:24 +0200

    fixup! Twelve: Fix post calls
    Change-Id: I76a1867cd6905e3909e91b321683415b8d9ab2b6

--- app/src/main/java/org/lineageos/twelve/datasources/jellyfin/JellyfinClient.kt
+++ app/src/main/java/org/lineageos/twelve/datasources/jellyfin/JellyfinClient.kt
@@ -234,7 +234,7 @@ class JellyfinClient(
         ),
     ).execute(api).mapToError()
 
-    suspend fun addItemToPlaylist(id: UUID, audioId: UUID) = ApiRequest.post<Any, Unit>(
+    suspend fun addItemToPlaylist(id: UUID, audioId: UUID) = ApiRequest.post<Unit, Unit>(
         listOf(
             "Playlists",
             id.toString(),
@@ -264,7 +264,7 @@ class JellyfinClient(
         ),
     ).execute(api).mapToError()
 
-    suspend fun addToFavorites(id: UUID) = ApiRequest.post<Any, Unit>(
+    suspend fun addToFavorites(id: UUID) = ApiRequest.post<Unit, Unit>(
         listOf(
             "UserFavoriteItems",
             id.toString(),
