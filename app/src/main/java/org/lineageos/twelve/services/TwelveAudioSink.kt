42c37cb90ea5fa1b495757a0bef9facb436dbad0
Luca Stefani <luca.stefani.ge1@gmail.com>
2026-03-20 17:51:20 +0100

    Twelve: Extract AudioDeviceInfo in a saner way
    Doing it every on every handleBuffer and flush was stupid
    
    Change-Id: I5fbedcc7c12605cadea698f96529865095c2efed

--- app/src/main/java/org/lineageos/twelve/services/TwelveAudioSink.kt
+++ app/src/main/java/org/lineageos/twelve/services/TwelveAudioSink.kt
@@ -6,7 +6,10 @@
 package org.lineageos.twelve.services
 
 import android.media.AudioDeviceInfo
+import android.media.AudioRouting
 import android.media.AudioTrack
+import android.os.Handler
+import android.os.Looper
 import androidx.media3.common.Format
 import androidx.media3.common.util.Clock
 import androidx.media3.exoplayer.analytics.PlayerId
@@ -15,16 +18,15 @@ import androidx.media3.exoplayer.audio.AudioOutputProvider
 import androidx.media3.exoplayer.audio.AudioSink
 import androidx.media3.exoplayer.audio.AudioTrackAudioOutput
 import androidx.media3.exoplayer.audio.DefaultAudioSink
-import java.nio.ByteBuffer
 
 /**
- * An [AudioSink] implementation that wraps a [DefaultAudioSink] and exposes the [AudioTrack] used
- * by the [DefaultAudioSink].
+ * An [AudioSink] implementation that wraps a [DefaultAudioSink] and exposes the routed
+ * [AudioDeviceInfo] used by the [DefaultAudioSink].
  */
 @androidx.media3.common.util.UnstableApi
 class TwelveAudioSink(
     private val defaultAudioSink: DefaultAudioSink,
-    private val onAudioTrackUpdate: (AudioTrack?) -> Unit,
+    private val onAudioDeviceInfoChanged: (AudioDeviceInfo?) -> Unit,
 ) : AudioSink by defaultAudioSink {
     private val audioOutputField = DefaultAudioSink::class.java.getDeclaredField(
         "audioOutput"
@@ -42,6 +44,56 @@ class TwelveAudioSink(
             audioTrackField.get(it) as AudioTrack?
         }
 
+    private var externalListener: AudioSink.Listener? = null
+
+    private val handler = Handler(Looper.getMainLooper())
+
+    private val routingListener = AudioRouting.OnRoutingChangedListener { routing ->
+        onAudioDeviceInfoChanged(routing.routedDevice)
+    }
+
+    private var currentAudioTrack: AudioTrack? = null
+
+    init {
+        defaultAudioSink.setListener(object : AudioSink.Listener {
+            override fun onAudioTrackInitialized(audioTrackConfig: AudioSink.AudioTrackConfig) {
+                currentAudioTrack?.removeOnRoutingChangedListener(routingListener)
+
+                val track = audioTrack
+                currentAudioTrack = track
+
+                track?.addOnRoutingChangedListener(routingListener, handler)
+                onAudioDeviceInfoChanged(track?.routedDevice)
+
+                externalListener?.onAudioTrackInitialized(audioTrackConfig)
+            }
+
+            override fun onAudioTrackReleased(audioTrackConfig: AudioSink.AudioTrackConfig) {
+                if (currentAudioTrack == null) {
+                    onAudioDeviceInfoChanged(null)
+                }
+
+                externalListener?.onAudioTrackReleased(audioTrackConfig)
+            }
+
+            override fun onPositionDiscontinuity() {
+                externalListener?.onPositionDiscontinuity()
+            }
+
+            override fun onUnderrun(p0: Int, p1: Long, p2: Long) {
+                externalListener?.onUnderrun(p0, p1, p2)
+            }
+
+            override fun onSkipSilenceEnabledChanged(p0: Boolean) {
+                externalListener?.onSkipSilenceEnabledChanged(p0)
+            }
+        })
+    }
+
+    override fun setListener(listener: AudioSink.Listener) {
+        externalListener = listener
+    }
+
     override fun setPlayerId(playerId: PlayerId?) {
         defaultAudioSink.setPlayerId(playerId)
     }
@@ -52,16 +104,6 @@ class TwelveAudioSink(
 
     override fun getFormatOffloadSupport(format: Format): AudioOffloadSupport {
         return defaultAudioSink.getFormatOffloadSupport(format)
-    }
-
-    override fun handleBuffer(
-        buffer: ByteBuffer,
-        presentationTimeUs: Long,
-        encodedAccessUnitCount: Int,
-    ) = try {
-        defaultAudioSink.handleBuffer(buffer, presentationTimeUs, encodedAccessUnitCount)
-    } finally {
-        onAudioTrackUpdate(audioTrack)
     }
 
     override fun setPreferredDevice(audioDeviceInfo: AudioDeviceInfo?) {
@@ -84,13 +126,12 @@ class TwelveAudioSink(
         defaultAudioSink.setAudioOutputProvider(audioOutputProvider)
     }
 
-    override fun flush() = try {
+    override fun flush() {
         defaultAudioSink.flush()
-    } finally {
-        onAudioTrackUpdate(null)
     }
 
     override fun release() {
+        audioTrack?.removeOnRoutingChangedListener(routingListener)
         defaultAudioSink.release()
     }
 }
