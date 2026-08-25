eceb485ab1ea85494fd8f505b9e5563055ecb16f
Luca Stefani <luca.stefani.ge1@gmail.com>
2026-08-25 22:54:35 +0200

    Twelve: Use ExoPlayer.setTrackSelector()
    Change-Id: Ia078b56c81fded1eab3672a6cc83208fe6aca75b

--- app/src/main/java/org/lineageos/twelve/services/PlaybackService.kt
+++ app/src/main/java/org/lineageos/twelve/services/PlaybackService.kt
@@ -20,11 +20,13 @@ import androidx.media3.common.HeartRating
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.common.Rating
+import androidx.media3.common.TrackSelectionParameters
 import androidx.media3.common.listen
 import androidx.media3.common.util.ExperimentalApi
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.analytics.AnalyticsListener
+import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
 import androidx.media3.session.CommandButton
 import androidx.media3.session.DefaultMediaNotificationProvider
 import androidx.media3.session.LibraryResult
@@ -50,7 +52,6 @@ import org.lineageos.twelve.ext.enableOffload
 import org.lineageos.twelve.ext.mapAsync
 import org.lineageos.twelve.ext.mediaItems
 import org.lineageos.twelve.ext.next
-import org.lineageos.twelve.ext.setOffloadEnabled
 import org.lineageos.twelve.ext.skipSilence
 import org.lineageos.twelve.ext.stopPlaybackOnTaskRemoved
 import org.lineageos.twelve.ext.typedRepeatMode
@@ -375,7 +376,20 @@ class PlaybackService : MediaLibraryService() {
             when (CustomCommand.fromCustomAction(customCommand.customAction)) {
                 CustomCommand.TOGGLE_OFFLOAD -> {
                     args.getBoolean(CustomCommand.ARG_VALUE).let {
-                        player.setOffloadEnabled(it)
+                        player.trackSelectionParameters =
+                            player.trackSelectionParameters.buildUpon()
+                                .setAudioOffloadPreferences(
+                                    TrackSelectionParameters.AudioOffloadPreferences
+                                        .Builder()
+                                        .setAudioOffloadMode(
+                                            if (it) {
+                                                TrackSelectionParameters.AudioOffloadPreferences.AUDIO_OFFLOAD_MODE_ENABLED
+                                            } else {
+                                                TrackSelectionParameters.AudioOffloadPreferences.AUDIO_OFFLOAD_MODE_DISABLED
+                                            }
+                                        )
+                                        .build()
+                                ).build()
                     }
 
                     SessionResult(SessionResult.RESULT_SUCCESS)
@@ -443,13 +457,25 @@ class PlaybackService : MediaLibraryService() {
                     },
                 )
             )
+            .setTrackSelector(DefaultTrackSelector(this).apply {
+                setParameters(
+                    buildUponParameters()
+                        .setAudioOffloadPreferences(
+                            TrackSelectionParameters.AudioOffloadPreferences.Builder()
+                                .setAudioOffloadMode(
+                                    if (sharedPreferences.enableOffload) {
+                                        TrackSelectionParameters.AudioOffloadPreferences.AUDIO_OFFLOAD_MODE_ENABLED
+                                    } else {
+                                        TrackSelectionParameters.AudioOffloadPreferences.AUDIO_OFFLOAD_MODE_DISABLED
+                                    }
+                                ).build()
+                        )
+                )
+            })
             .setSkipSilenceEnabled(sharedPreferences.skipSilence)
             .setWakeMode(C.WAKE_MODE_NETWORK)
             .experimentalSetDynamicSchedulingEnabled(true)
             .build()
-            .apply {
-                setOffloadEnabled(sharedPreferences.enableOffload)
-            }
 
         mediaLibrarySession = MediaLibrarySession.Builder(
             this, player, mediaLibrarySessionCallback
